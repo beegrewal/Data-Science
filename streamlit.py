0e285c499dc068e85146028fff7ec44ce2ecb08d
B S GREWAL <123067840+beegrewal@users.noreply.github.com>
2023-10-23 11:07:21 +0530

    First commit

---
+++ streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st  
+import pandas as pd
+import sklearn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+      
+
+
+df = pd.read_csv('Usedcar.csv')
+st.title('Used Cars')
+col1,col2,col3 = st.columns(3)
+col1.metric('Luxury Sedan','','')
+col2.metric('Hatchback','','') 
+
+if st.sidebar.button('Load Dataset'):
+    st.write(df)
+    df1 =df.head(20)
+    fig = plt.figure(figsize=(10,8))
+    plt.bar(df['Brand'],df['Price'])
+    st.pyplot(fig)
+
+ 
+
+
